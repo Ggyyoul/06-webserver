014604aee6a842da7d450c413ee9f024248182a9
Juan Manuel Palacios <juan980424@gmail.com>
2021-07-02 10:46:37 -0500

    serve a complete page in express

--- index.js
+++ index.js
@@ -5,10 +5,17 @@ const port = 8080;
 // Servir contenido estatico
 app.use(express.static('public'));
 
-app.get('/hola-mundo', (req, res) => {
-  res.send('Helloooo');
+app.get('/index', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html')
 });
 
+app.get('/generic', (req, res) => {
+  res.sendFile(__dirname + '/public/generic.html')
+});
+
+app.get('/elements', (req,res) => {
+  res.sendFile(__dirname + '/public/elements.html')
+})
 app.get('*', (req, res) => {
   res.sendFile(__dirname + '/public/404.html');
 });
