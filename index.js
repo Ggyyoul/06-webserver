fa0693697561e478aa82709cf8d2e4d940c21f23
Juan Manuel Palacios <juan980424@gmail.com>
2021-07-02 10:07:12 -0500

    express introduction

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
-const http = require('http');
+const express = require('express')
+const app = express()
+ 
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+ 
+app.get('/hola-mundo', function (req, res) {
+  res.send('Helloooo')
+})
 
-// la request es lo que se me esta solicitando al web server y respose es lo que mi servidores responderá al cliente
-http
-  .createServer((request, response) => {
-    response.setHeader('Content-Disposition', 'attachment; filename=list.csv');
-    response.writeHead(200, { 'Content-Type': 'application/csv' });
-
-    response.write('id,nombre\n');
-    response.write('1, Juan\n');
-    response.write('2, manuel\n');
-    response.write('3, palacios\n');
-    response.write('4, javier\n');
-
-    response.end();
-  })
-  .listen(8080);
-console.log('escuchando en el puerto', 8080);
+app.get('*', function (req, res) {
+  res.send('Error 404 page not found')
+})
+app.listen(3000)
